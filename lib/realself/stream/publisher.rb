4c13b3e85ee7da25c30dd2e12dc184458f465a67
Sam Leavens <sam@realself.com>
2013-09-17 15:16:52 -0700

    Symbolizing keys when configuring Bunny

--- lib/realself/stream/publisher.rb
+++ lib/realself/stream/publisher.rb
@@ -5,7 +5,7 @@ module RealSelf
     class Publisher
       class << self
         def configure(config, exchange_name)
-          @@publisher_session = Bunny.new(config)
+          @@publisher_session = Bunny.new(symbolize_keys(config))
           @@publisher_session.start
           @@publisher_channel = @@publisher_session.create_channel
           @@publisher_exchange = @@publisher_channel.topic(exchange_name, :durable => true)
@@ -20,6 +20,21 @@ module RealSelf
             :persistent => true,
             :routing_key => "#{item.actor.type}.#{item.verb}.#{item.object.type}"
           )
+        end
+
+        def symbolize_keys(hash)
+          hash.inject({}){|result, (key, value)|
+            new_key = case key
+                      when String then key.to_sym
+                      else key
+                      end
+            new_value = case value
+                        when Hash then symbolize_keys(value)
+                        else value
+                        end
+            result[new_key] = new_value
+            result
+          }
         end
       end
     end
