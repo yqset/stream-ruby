15a20d0dda93ba2fda58e2681238bf35f0c29682
Sam Leavens <sam@realself.com>
2013-12-03 16:31:57 -0800

    Adding RoutingKey to Stream

--- lib/realself/stream.rb
+++ lib/realself/stream.rb
@@ -1,11 +1,12 @@
-require "realself/stream/activity"
-require "realself/stream/error"
-require "realself/stream/objekt"
-require "realself/stream/followed_activity"
-require "realself/stream/followed_objekt"
-require "realself/stream/publisher"
-require "realself/stream/stream_activity"
-require "realself/stream/version"
+require 'realself/stream/activity'
+require 'realself/stream/error'
+require 'realself/stream/objekt'
+require 'realself/stream/followed_activity'
+require 'realself/stream/followed_objekt'
+require 'realself/stream/publisher'
+require 'realself/stream/stream_activity'
+require 'realself/stream/routing_key'
+require 'realself/stream/version'
 
 module Realself
   module Stream
