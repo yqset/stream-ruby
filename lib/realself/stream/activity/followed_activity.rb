70665e711b3fe5f18692f7bb35185e5d86e884b4
Matt Towers <matt@realself.com>
2014-07-02 12:13:58 -0700

    remove unused code

--- lib/realself/stream/activity/followed_activity.rb
+++ lib/realself/stream/activity/followed_activity.rb
@@ -1,38 +1,8 @@
-require 'json-schema'
-# require 'realself/stream/activity'
-# require 'realself/stream/objekt'
-# require 'realself/stream/followed_objekt'
-# require 'realself/stream/stream_activity'
-
 module RealSelf
   module Stream
     class FollowedActivity < Activity
 
       class << self   
-        # @@schema = nil
-
-        # def create(version, *args)
-        #   klass = get_implementation(version)
-        #   klass.new(*args)
-        # end
-
-        # def from_hash(hash)
-        #   version = hash[:version] || 1
-        #   klass = get_implementation(version)
-        #   klass.from_hash(hash)
-        # end
-
-
-        # def from_json(json, validate = true)
-        #   if validate
-        #     @@schema = @@schema || MultiJson.decode(open(@@schema_file).read)
-        #     JSON::Validator.validate!(@@schema, json)
-        #   end
-
-        #   hash = MultiJson.decode(json, {:symbolize_keys => true})
-        #   from_hash(hash)
-        # end
-
         private 
 
         def get_implementation(version)
@@ -44,16 +14,6 @@ module RealSelf
           end
         end
       end # class << self
-
-      # def ==(other)
-      #   self.to_h == other.to_h
-      # end
-
-      # alias :eql? :==
-
-      # def to_s
-      #   MultiJson.encode(self.to_h)
-      # end
     end # FollowedActivityV1
   end # Stream
 end # RealSelf
