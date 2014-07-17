8451dbd7f240913777af3cb67b0a253fb5d59458
Matt Towers <matt@realself.com>
2014-07-17 16:34:51 -0700

    don't attempt create digests for user data type - i.e. when the stream_activity reason is the user who owns the stream activity.

--- lib/realself/stream/digest/digest.rb
+++ lib/realself/stream/digest/digest.rb
@@ -49,8 +49,12 @@ module RealSelf
           end
 
           stream_activity.reasons.each do |reason|
-            summary = get_summary(reason)
-            summary.add(stream_activity)
+            # don't add digest summaries if the stream_activity reason
+            # is the owner of the stream_activity (meaning the owner is following themselves)
+            unless @owner == reason
+              summary = get_summary(reason)
+              summary.add(stream_activity)
+            end
           end
         end
 
