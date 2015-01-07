2cd7d21da8b6ba5e50ba7301eb0a196edecee71c
Matt Towers <matt@realself.com>
2015-01-07 14:28:46 -0800

    ROADMAP-1202 formatting fix

--- lib/realself/stream/content_type.rb
+++ lib/realself/stream/content_type.rb
@@ -1,11 +1,11 @@
 module RealSelf
   module Stream
     class ContentType
-      DIGEST_ACTIVITY = 'application/com.realself.digest_activity+json'
-      ACTIVITY = 'application/com.realself.activity+json'
-      STREAM_ACTIVITY = 'application/com.realself.stream_activity+json'
+      ACTIVITY          = 'application/com.realself.activity+json'
+      DIGEST_ACTIVITY   = 'application/com.realself.digest_activity+json'
       FOLLOWED_ACTIVITY = 'application/com.realself.followed_activity+json'
-      OBJEKT = 'application/com.realself.objekt+json'
+      OBJEKT            = 'application/com.realself.objekt+json'
+      STREAM_ACTIVITY   = 'application/com.realself.stream_activity+json'
     end
   end
 end
