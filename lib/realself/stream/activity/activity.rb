52df2168e3c65b49fa857ea7c385a760a78db7f3
Matt Towers <matt@realself.com>
2015-02-17 13:31:00 -0800

    ROADMAP-2353 Add V4 UUID regex to Activity class

--- lib/realself/stream/activity/activity.rb
+++ lib/realself/stream/activity/activity.rb
@@ -6,6 +6,8 @@ module RealSelf
   module Stream
     class Activity
 
+      UUID_REGEX = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/
+
       class << self
         @@schema = nil
 
--- lib/realself/stream/version.rb
+++ lib/realself/stream/version.rb
@@ -1,5 +1,5 @@
 module Realself
   module Stream
-    VERSION = '0.3.0'
+    VERSION = '0.3.1'
   end
 end
