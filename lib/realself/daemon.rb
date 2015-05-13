7540c910e66c410d0f41f857b362c7840aaa5c9a
Matt Towers <matt@realself.com>
2015-05-13 14:57:11 -0700

    ROADMAP-2738 Add logger singleton

--- lib/realself/daemon.rb
+++ lib/realself/daemon.rb
@@ -7,8 +7,10 @@ require 'sneakers'
 require 'sneakers/worker'
 require 'realself/content_type'
 require 'realself/handler'
-require 'realself/daemon/worker'
 
+require 'realself/daemon/worker'
 require 'realself/daemon/activity_worker'
 require 'realself/daemon/digest_worker'
 require 'realself/daemon/stream_activity_worker'
+
+require 'realself/logger'
