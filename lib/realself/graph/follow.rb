8553f8d570fc97fb798d3de9ba07c83cc8dd0dd5
Matt Towers <matt@realself.com>
2015-11-13 16:09:57 -0800

    Add method documentation to Follow graph methods

--- lib/realself/graph/follow.rb
+++ lib/realself/graph/follow.rb
@@ -1,18 +1,32 @@
 module RealSelf
   module Graph
     module Follow
-
-
-      def self.configure mongo_client
-        @mongo_db = mongo_client
+      ##
+      # sets the MongoDB to use
+      #
+      # @param [Mongo::Database] The database to use
+      def self.configure mongo_db
+        @mongo_db = mongo_db
       end
 
 
+      ##
+      # count the number of followers of a specified type of a given object
+      #
+      # @param [String] The type of actors doing the following e.g. :user
+      # @param [Objekt] The item for which the number of followers should be counted
+      #
+      # @return [Integer] The number of followers
       def self.count_followers_of actor_type, object
         get_collection(actor_type).find({:'object' => object.to_h}).count
       end
 
 
+      ##
+      # create indexes on the collection if necessary
+      #
+      # @param [String] owner_type  The type of actor that is doing the following
+      # @param [true | false]       Create the index in the background
       def self.ensure_index(actor_type, background = true)
         get_collection(actor_type).indexes.create_many([
           {
@@ -33,6 +47,16 @@ module RealSelf
       end
 
 
+      ##
+      # test if an actor is following one or more objects.  If the
+      # actor is following any of the passed objects, they will be
+      # included as an element in the returned array
+      #
+      # @param [Objekt]             actor   The follower
+      # @param [Objekt | Array]     objects An Objekt or array of Objeks to test
+      # @param [&block] (optional)  a block to execute for each Objekt in the result
+      #
+      # @return [Array] An array containing zero or more of the passed objects
       def self.is_following actor, objects
         objects = [*objects].map {|obj| obj.to_h}
 
@@ -51,6 +75,11 @@ module RealSelf
       end
 
 
+      ##
+      # create a follow relationship
+      #
+      # @param [Objekt] actor The follower
+      # @param [Objekt] object The item being followed
       def self.follow actor, object
         upsert_query = {:'actor.id' => actor.id, :'object' => object.to_h}
 
@@ -61,6 +90,13 @@ module RealSelf
       end
 
 
+      ##
+      # gets the objects that an actor is following
+      #
+      # @param [Objekt] The follower
+      # @param [&block] (optional) A block to execute for each Objekt being followed
+      #
+      # @return (Array) An array of Objekts that the actor is following
       def self.followed_by actor
         cursor = get_collection(actor.type).find(
           {:'actor.id' => actor.id}, # query
@@ -77,6 +113,14 @@ module RealSelf
       end
 
 
+      ##
+      # gets the objects that an actor is following
+      #
+      # @param [String] The type of actors to include e.g. :user
+      # @param [Objekt] The object being followed
+      # @param [&block] (optional) A block to execute for each actor following the object
+      #
+      # @return (Array) An array of Objekts containing the actors
       def self.followers_of actor_type, object
         cursor = get_collection(actor_type).find(
           {:object => object.to_h},
@@ -92,7 +136,13 @@ module RealSelf
         end
       end
 
-
+      ##
+      # destroy a follow relationship
+      #
+      # @param [Objekt] actor The follower
+      # @param [Objekt] object The item being followed
+      #
+      # @return [Bool] true if the follow relationship existed and was deleted
       def self.unfollow actor, object
         1 == get_collection(actor.type).delete_one({:'actor.id' => actor.id, :'object.id' => object.id}).n
       end
