d02a2d310ab3dbebede825f066a9c7e785363ca7
Ying Qing Set <set.yingqing@realself.com>
2016-06-23 10:50:07 -0700

    Ordering of require actually matters

--- lib/realself/feed.rb
+++ lib/realself/feed.rb
@@ -3,7 +3,16 @@ module RealSelf
   end
 end
 
-Dir[File.dirname(__FILE__) + '/feed/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/feed/state/*.rb'].each {|file| require file }
+require 'realself/feed/capped'
+require 'realself/feed/feed_error'
+require 'realself/feed/getable'
+require 'realself/feed/permanent'
+require 'realself/feed/redactable'
+require 'realself/feed/ttl'
+require 'realself/feed/unread_countable'
+require 'realself/feed/stateful'
+require 'realself/feed/state/bookmarkable'
+require 'realself/feed/state/sessioned'
+require 'realself/feed/state/unread_countable'
 require 'realself/logger'
 require 'realself/stream/activity'
